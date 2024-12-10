5323fe2f9d0cdefda599b4796ef217e1f0f5eac3
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-12-10 13:58:15 +0330

    feat(frontend): add favicon to RootLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,6 +26,7 @@ const RootLayout: React.FC<AuthLayoutProps> = ({ children }) => {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
+                <link rel="icon" href="/images/favicon.webp" />
             </head>
             <body
                 className={`${vazir.className}`}
