81e6d6fdb204452119b9b6beb0ed98f30bfe07cf
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-12-10 12:37:22 +0330

    feat: update AppModule with additional modules (Auth, Users, Colors, Categories, Banners, Products, Brands, Orders, Discount)

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '@modules/database/database.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { UsersModule } from '@modules/users/users.module';
+import { ColorsModule } from '@modules/colors/colors.module';
+import { CategoryModule } from '@modules/categories/categories.module';
+import { BannersModule } from '@modules/banners/banners.module';
+import { ProductModule } from '@modules/products/products.module';
+import { BrandModule } from '@modules/brands/brands.module';
+import { OrdersModule } from '@modules/orders/orders.module';
+import { DiscountModule } from '@modules/discount/discount.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    ColorsModule,
+    CategoryModule,
+    BannersModule,
+    ProductModule,
+    BrandModule,
+    OrdersModule,
+    DiscountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
